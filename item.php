4c4ca1f0ff969087346ee3c44bf7008cdd29cd58
bbuussaaccoo <busacco@gmail.com>
2016-07-25 22:26:21 -0700

    changed syntax

--- item.php
+++ item.php
@@ -1,26 +1,21 @@
 <?php
 //item.php
 
+
 class item
 {
     public $ItemName = '';
     public $ItemDescription ='';
     public $ItemPrice = '15.00';
    
-    function __construct($name, $description, $price)
+    function __construct($ItemName, $ItemDescription, $ItemPrice)
     {
-        $this->ItemName = $name;
-        $this->ItemDescription = $description;
-        $this->itemPrice = $price;
+        $this->ItemName = $ItemName;
+        $this->ItemDescription = $ItemDescription;
+        $this->ItemPrice = $ItemPrice;
     }
     
-  /*  function GetItem()
-    {
-        echo $this->PizzaName = $name."<br>".
-        $this->PizzaDescription = $description."<br>".
-        $this->PizzaPrice = $price;
-    }*/
-    
 }
-
 ?>
+
+
